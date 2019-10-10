8836f23f52574621cd15732a443f56d96d496b34
Timo <timo.tiirats@tudeng.ut.ee>
2019-10-10 22:48:36 +0300

    Task 1 and 2 should be done

--- res/js/script.js
+++ res/js/script.js
@@ -1,16 +1,39 @@
 $(function () {
-    var user = new user("John","Doe","11/10/1990","Software Engineering","2.75")
+    // User object with random data
+    var user = new User("Donald","Duck","27/03/1995","Computer Engineering","3.75")
+    // Array of courses objects
     var courses = [
-        new Item("Agile software development", 1, 82),
-        new Item("System modeling", 1, 85),
-        new Item("Object-oriented programming", 2, 99),
-        new Item("Estonian language level A2", 2, ),
+        new Courses("Robotics", 1, 78),
+        new Courses("Algorithmics", 2, 75),
+        new Courses("Computer Graphics", 3, 100),
+        new Courses("Business Analysis", 1, 56),
     ];
     init();
-
+   
     
 
     function init() {
-        // Something smart need to be done here
+        // Personal information
+        $("#name").text(user.firstname + " " + user.lastname);
+        $("#birthdate").text(user.birthdate);
+        $("#faculty").text(user.faculty);
+        $("#gpa strong").text(user.gpa);
+        
+        // Courses information
+        for (let i = 0; i < courses.length; i++) {
+            let tr = $("<tr></tr>");
+            let nr = $("<td></td>").text(i+1);
+            let course = $("<td></td>").text(courses[i].title);
+            let semester = $("<td></td>").text(courses[i].semester);
+            let grade = $("<td></td>").text(courses[i].grade);
+
+            tr.append(nr);
+            tr.append(course);
+            tr.append(semester);
+            tr.append(grade);
+
+            // Adding table row to table
+            $("#courses tbody").append(tr);
+        }
     }
 });
