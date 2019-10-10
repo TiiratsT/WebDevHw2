6f881c5a68df1da97f08d424ac12c4b6472253d8
Timo Tiirats <timo.tiirats@gmail.com>
2019-10-10 09:55:09 +0300

    1,75 tasks done

---
+++ res/js/script.js
@@ -0,0 +1,16 @@
+$(function () {
+    var user = new user("John","Doe","11/10/1990","Software Engineering","2.75")
+    var courses = [
+        new Item("Agile software development", 1, 82),
+        new Item("System modeling", 1, 85),
+        new Item("Object-oriented programming", 2, 99),
+        new Item("Estonian language level A2", 2, ),
+    ];
+    init();
+
+    
+
+    function init() {
+        // Something smart need to be done here
+    }
+});
